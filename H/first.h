c8435300cc290ef1f0cb13e412793d4ecffdd07b
Robin <Robin>
2021-03-07 18:02:43 -0500

    Changes in release branch

--- H/first.h
+++ H/first.h
@@ -3,3 +3,4 @@
 #define new_define 400
 #define release_define "release"
 #define release_after_featurebranch1
+#define val "fresh changes to test rebase in a common module - This change is in release branch"
